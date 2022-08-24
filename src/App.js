46b22c6501ae598b7ada2acad53668bb7878ce41
HederAlves <alvesheder92@gmail.com>
2022-08-24 11:26:28 -0300

    Update
    App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Header } from "./components/Header/Header"
+import { Footer } from "./components/Footer/Footer"
 import { Carousel } from "./components/Carousel/Carousel"
- 
+import {Banner} from "./components/Banner/Banner"
+
 export default function App() {
 
   return (
     <>
       <Header></Header>
+      <Banner></Banner>
       <Carousel></Carousel>
+      <Footer></Footer>
     </>
   )
 }
