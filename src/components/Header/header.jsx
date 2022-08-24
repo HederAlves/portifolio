8325cddd6106ff0297c2fe7761f9b69508ac67ee
HederAlves <alvesheder92@gmail.com>
2022-08-24 11:27:25 -0300

    Update
    Component Header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -12,16 +12,13 @@ import iconInstagram from '../../assets/icon/icon-instagram.png'
     }
 
     return (
-
       <>
-      <header className='h-32 bg-black flex justify-around items-center'>
-        <img className="border-2 border-orange-900 bg-gradient-to-r from-yellow-400 via-orange-500 via-red-600 to-fuchsia-600 rounded-full w-24 h-24 p-1" src={imgPerfil} alt="Desenho de colaboradores" />
-        <ul>
-          <li><button>Home</button></li>
-          <li><button>Projetos</button></li>
-          <li><button>Tutoria</button></li>
-          <li><button>Sobre</button></li>
-        </ul>
+      <header className='h-20 bg-black flex justify-between items-center px-10 pt-5'>
+        <div className='flex items-center justify-around w-[350px]'>
+        <img className="border-2 border-orange-900 bg-gradient-to-r from-yellow-400 via-orange-500 via-red-600 to-fuchsia-600 rounded-full w-16 h-14 p-1 m-5" src={imgPerfil} alt="Desenho de colaboradores" />
+        <h1 className='font-extrabold text-orange-500 text-2xl'>Desenvolvedor Front-end</h1>
+        </div>
+
         <ul className='flex justify-around w-52 items-center'>
           <li><a href={user.linkedin}><img className="border-2 border-orange-900 hover:bg-gradient-to-r from-yellow-400 via-orange-500 via-red-600 to-fuchsia-600 rounded-full w-12 p-2" src={iconLinkedin} alt="Desenho de colaboradores" /></a></li>
           <li><a href={user.github}><img className="border-2 border-orange-900 hover:bg-gradient-to-r from-yellow-400 via-orange-500 via-red-600 to-fuchsia-600 rounded-full  w-12 p-2" src={iconGithub} alt="Desenho de colaboradores" /></a></li>
