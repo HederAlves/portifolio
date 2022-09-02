99215bf29bf067e342b2aee3621dcd0fdc6b717f
HederAlves <alvesheder92@gmail.com>
2022-09-01 22:01:22 -0300

    Update
    Footer, mobile

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -3,34 +3,15 @@ import "./style.css"
 export function Footer(props) {
   return (
     <>
-      <footer className=" bg-black px-10 pb-5">
+      <footer className="containerMainFooter">
         <h1 className="font-extrabold text-cyan-400 text-center bg-black py-4 text-3xl">
           Sobre mim
         </h1>
-        <section className="flex">
-          <article className="w-1/3 text-white pl-5">
-            <h2 className="font-extrabold text-lime-500 text-center">
-              SoftSkills
-            </h2>
-            <ul className="mt-5 flex justify-around">
-              <div className="flex flex-col gap-2">
-                <li>- Inteligência emocional.</li>
-                <li>- Resolução de problemas.</li>
-                <li>- Pensamento Crítico.</li>
-                <li>- Tomada de decisão.</li>
-              </div>
-              <div className="flex flex-col gap-2">
-                <li>- Criatividade.</li>
-                <li>- Organização.</li>
-                <li>- Comunicação.</li>
-                <li>- Flexibilidade.</li>
-              </div>
-            </ul>
-          </article>
-
-          <article className="text-white border-x-2 border-orange-500 w-1/3 h-[410px] pl-5">
-            <h2 className="font-extrabold text-lime-500 text-center"></h2>
-            <p className="text-white pr-5 mt- text-justify">
+        <section className="flex flex-col xl:flex-row gap-5 xl:gap-0">
+            
+          <article className="text-white  xl:h-[410px] xl:w-2/3">
+            <h2 className="font-extrabold text-lime-500 text-center mb-5"> Minha Historia</h2>
+            <p className="text-white px-5 text-justify">
               Desde criança sempre fui muito ligado a tecnologia, acompanhei
               grande parte de seu avanço, principalmente com internet e jogos,
               onde sempre estava conectado. Após anos atuando na administração,
@@ -46,6 +27,25 @@ export function Footer(props) {
               desenvolvedor Front-end.
             </p>
           </article>
+          <article className="xl:w-1/3 text-white pl-5">
+          <h2 className="font-extrabold text-lime-500 text-center mt-2 xl:mt-0">
+              SoftSkills
+            </h2>
+            <ul className="mt-5 flex xl:justify-around gap-5 xl:gap-0">
+              <div className="flex flex-col gap-2">
+                <li>Inteligência emocional.</li>
+                <li>Resolução de problemas.</li>
+                <li>Pensamento Crítico.</li>
+                <li>Tomada de decisão.</li>
+              </div>
+              <div className="flex flex-col gap-2">
+                <li>Criatividade.</li>
+                <li>Organização.</li>
+                <li>Comunicação.</li>
+                <li>Flexibilidade.</li>
+              </div>
+            </ul>
+          </article>
 
           <article className="articleRigth">
             <h2 className="titleArticleRigth">
@@ -53,19 +53,19 @@ export function Footer(props) {
             </h2>
             <ul className="ulRigth">
               <div className="divRigth">
-                <li>- React.js.</li>
-                <li>- Node.js.</li>
-                <li>- Git.</li>
+                <li>React.js.</li>
+                <li>Node.js.</li>
+                <li>Git.</li>
               </div>
               <div className="divRigth">
-                <li>- JavaScript.</li>
-                <li>- HTML.</li>
-                <li>- CSS.</li>
+                <li>JavaScript.</li>
+                <li>HTML.</li>
+                <li>CSS.</li>
               </div>
               <div className="divRigth">
-                <li>- Vue.js.</li>
-                <li>- TailwindCSS.</li>
-                <li>- SQL.</li>
+                <li>Vue.js.</li>
+                <li>TailwindCSS.</li>
+                <li>SQL.</li>
               </div>
             </ul>
           </article>
