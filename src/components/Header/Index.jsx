e47780228bc6b681a65b7365ba07a5da9f971819
HederAlves <alvesheder92@gmail.com>
2022-09-01 22:01:48 -0300

    Update
    Header, mobile

--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -3,6 +3,8 @@ import iconLinkedin from '../../assets/icon/icon-linkedin.png'
 import iconGithub from '../../assets/icon/icon-github.png'
 import iconInstagram from '../../assets/icon/icon-instagram.png'
 
+import "./style.css";
+
   export function Header  (props)  {
 
     var user = {
@@ -13,18 +15,17 @@ import iconInstagram from '../../assets/icon/icon-instagram.png'
 
     return (
       <>
-      <header className='h-20 bg-black flex justify-between items-center px-10 pt-5'>
-        <div className='flex items-center justify-around w-[350px]'>
-        <img className="border-2 border-orange-900 bg-gradient-to-r from-yellow-400 via-orange-500 via-red-600 to-fuchsia-600 rounded-full w-16 h-14 p-1 m-5" src={imgPerfil} alt="Desenho de colaboradores" />
-        <h1 className='font-extrabold text-orange-500 text-2xl'>Desenvolvedor Front-end</h1>
+      <header className='containerMainHeader'>
+        <div className='containerTitleImage'>
+        <img className='imageHeader' src={imgPerfil} alt="imagem de perfil"/>
+        <h1 className='titleHeader'>Desenvolvedor Front-end</h1>
         </div>
 
-        <ul className='flex justify-around w-52 items-center'>
-          <li><a href={user.linkedin}><img className="border-2 border-orange-900 hover:bg-gradient-to-r from-yellow-400 via-orange-500 via-red-600 to-fuchsia-600 rounded-full w-12 p-2" src={iconLinkedin} alt="Desenho de colaboradores" /></a></li>
-          <li><a href={user.github}><img className="border-2 border-orange-900 hover:bg-gradient-to-r from-yellow-400 via-orange-500 via-red-600 to-fuchsia-600 rounded-full  w-12 p-2" src={iconGithub} alt="Desenho de colaboradores" /></a></li>
-          <li><a href={user.instagram}><img className="border-2 border-orange-900 hover:bg-gradient-to-r from-yellow-400 via-orange-500 via-red-600 to-fuchsia-600 rounded-full w-12 p-2" src={iconInstagram} alt="Desenho de colaboradores" /></a></li>
+        <ul className='containerContact'>
+          <li><a href={user.linkedin}><img className="imageContact" src={iconLinkedin} alt="Desenho de colaboradores" /></a></li>
+          <li><a href={user.github}><img className="imageContact" src={iconGithub} alt="Desenho de colaboradores" /></a></li>
+          <li><a href={user.instagram}><img className="imageContact" src={iconInstagram} alt="Desenho de colaboradores" /></a></li>
         </ul>
       </header>
     </>
-
     )}
